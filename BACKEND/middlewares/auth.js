0d644087870724a066a24b4f0151dbfe8a2d05a1
samparna kamble <144938616+Samparnakamble@users.noreply.github.com>
2025-03-17 17:32:31 +0530

    Update auth.js

--- BACKEND/middlewares/auth.js
+++ BACKEND/middlewares/auth.js
@@ -4,13 +4,38 @@ import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
 
+// export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+//   const { token } = req.cookies;
+
+//   if (!token) {
+//     return next(
+//       new ErrorHandler("User Not Authorized. No token provided.", 401)
+//     );
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+//     req.user = await User.findById(decoded.id);
+
+//     if (!req.user) {
+//       return next(new ErrorHandler("User does not exist. Access denied.", 401));
+//     }
+
+//     next();
+//   } catch (error) {
+//     return next(new ErrorHandler("Invalid or expired token.", 401));
+//   }
+// });
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const { token } = req.cookies;
+  let token = req.cookies.token;
+
+  // If token is not in cookies, check the Authorization header
+  if (!token && req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
   if (!token) {
-    return next(
-      new ErrorHandler("User Not Authorized. No token provided.", 401)
-    );
+    return next(new ErrorHandler("User Not Authorized. No token provided.", 401));
   }
 
   try {
@@ -26,3 +51,4 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Invalid or expired token.", 401));
   }
 });
+
