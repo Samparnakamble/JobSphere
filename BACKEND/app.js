187df83cb685078281d95895b1e587de29fd45a5
samparna kamble <144938616+Samparnakamble@users.noreply.github.com>
2025-03-17 16:51:13 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,68 +1,55 @@
-import { useContext, useEffect } from "react";
-import "./App.css";
-import { Context } from "./main";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Auth/Login";
-import Register from "./components/Auth/Register";
-import Navbar from "./components/Layout/Navbar";
-import Footer from "./components/Layout/Footer";
-import Home from "./components/Home/Home";
-import Jobs from "./components/Job/Jobs";
-import JobDetails from "./components/Job/JobDetails";
-import MyJobs from "./components/Job/MyJobs";
-import PostJobs from "./components/Job/PostJob";
-import Applications from "./components/Application/Application";
-import MyApplications from "./components/Application/MyApplication";
-import NotFound from "./components/NotFound/NotFound";
-import AdminPanel from "./components/AdminPanal/AdminPanel";
-import axios from "axios";
-import { Toaster } from "react-hot-toast";
-import AboutPage from "./components/AboutPage/AboutPage";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/userRouter.js";
+import jobRouter from "./routes/jobRouter.js";
+import adminRouter from "./routes/adminRouter.js";
+import applicationRoutes from "./routes/applicationRouter.js";
+import { dbConnection } from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import fileUpload from "express-fileupload";
+import visitRoutes from "./routes/visitRoutes.js";
+const app = express();
 
-const App = () => {
-  const { isAuthorized, setIsAuthorized, setUser } = useContext(Context);
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(
-          "https://jobsphere-frontend.onrender.com/api/v1/user/getuser",
-          {
-            withCredentials: true,
-          }
-        );
-        setUser(response.data.user);
-        setIsAuthorized(true);
-      } catch (error) {
-        console.error("Error fetching user:", error.response || error.message);
-        setIsAuthorized(false);
-      }
-    };
-    fetchUser();
-  }, [isAuthorized, setIsAuthorized, setUser]);
+// To give path for envorinment varibles
+dotenv.config({ path: "./config/config.env" });
 
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/job/getall" element={<Jobs />}></Route>
-          <Route path="/job/:id" element={<JobDetails />}></Route>
-          <Route path="/job/post" element={<PostJobs />}></Route>
-          <Route path="/job/me" element={<MyJobs />}></Route>
-          <Route path="/application/:id" element={<Applications />}></Route>
-          <Route path="/applications/me" element={<MyApplications />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-          <Route path="/adminpanel" element={<AdminPanel />}></Route>
-          <Route path="/aboutpage" element={<AboutPage />}></Route>
-        </Routes>
-        <Footer />
-        <Toaster />
-      </Router>
-    </>
-  );
+// In this the forntend is connect to backend in url form
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true,
 };
 
-export default App;
+// app.use is used for middlewares
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+// used to pars data in json format
+app.use(express.json());
+// used to convert string into json
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true, // Required for cloudinary
+    tempFileDir: "/tmp/",
+  })
+);
+
+// app.use(fileUpload({ useTempFiles: true }));
+
+const apiVersion = "/api/v1";
+
+app.use(`${apiVersion}/user`, userRouter);
+app.use(`${apiVersion}/application`, applicationRoutes);
+app.use(`${apiVersion}/job`, jobRouter);
+app.use(`${apiVersion}/admin`, adminRouter);
+app.use(`${apiVersion}/visit`, visitRoutes);
+
+dbConnection();
+
+app.use(errorMiddleware);
+
+export default app;
