599c20479b3fa6f586ac3bbf7d378f046e00780b
samparna kamble <144938616+Samparnakamble@users.noreply.github.com>
2025-03-17 16:03:42 +0530

    Update PostJob.jsx

--- FRONTEND/src/components/Job/PostJob.jsx
+++ FRONTEND/src/components/Job/PostJob.jsx
@@ -32,7 +32,7 @@ const PostJob = () => {
     }
     await axios
       .post(
-        "http://localhost:8000/api/v1/job/post",
+        "https://jobsphere-5mks.onrender.com/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
